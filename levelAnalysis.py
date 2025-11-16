291746d1c596d7a8a73234ef7afd168a00ffb6a2
klee1901 <kieranjameslee@hotmail.co.uk>
2025-11-16 18:29:45 +0000

    Refactor: Make levelAnalysis functional

--- levelAnalysis.py
+++ levelAnalysis.py
@@ -6,7 +6,39 @@ import datetime
 
 baseURL = "http://environment.data.gov.uk/hydrology"
 
-def dataNearTargetDate(stationID, targetDate):
+def getReadingsData(stationID):
+        
+    readingParams = {"station": stationID, "observedProperty": "waterLevel",
+        "mineq-date": "2023-01-01", "period": 86400, "valueType": "max"}
+    response = requests.get(baseURL+readingURL, params=readingParams)
+    if response.status_code == 200:
+        measuresData = pd.json_normalize(response.json()["items"])
+    else:
+        raise Exception("API error {0}".format(response.status_code))
+
+    return measuresData
+
+def cleanMeasuresData(rawMeasuresData):
+        
+    dayUIDs = rawMeasuresData.groupby("date").idxmax()["value"]
+    dayUIDs = dayUIDs.dropna()
+    
+    return rawMeasuresData.iloc[dayUIDs,:]
+
+def getLocalMaximums(measuresData, n=5):
+    
+    measuresData = measuresData.sort_values('date')
+    measuresData = measuresData.assign(
+        lastValue = lambda df: df.value.shift(1),
+        nextValue = lambda df: df.value.shift(-1)
+    ).assign(
+        localMax = lambda df: (df.value > df.lastValue) & (df.value > df.nextValue)
+    )
+    maximums = measuresData.query('localMax')
+    
+    return maximums.nlargest(n, 'value')
+
+def getDataNearTargetDate(stationID, targetDate):
 
     # stationID = nettlehamID
     # targetDate = dateOfInterest
@@ -31,45 +63,31 @@ def dataNearTargetDate(stationID, targetDate):
 
     return levelMeasures
 
-# Explore measurement data
-nettlehamID = "a941c6ca-6b2e-45b3-a515-198c39d4ceaa"
-stationURL = "/id/stations/"+nettlehamID+".json"
-stationURL = "/id/stations/"+nettlehamID+"/measures.json"
-nettlehamParams = {"station": nettlehamID}
-response = requests.get(baseURL+stationURL)#, params=nettlehamParams)
-print(json.dumps(response.json(), indent=2))
-# Readings
-readingURL = "/data/readings.json"
-readingParams = {"station": nettlehamID, "observedProperty": "waterLevel",
-    "mineq-date": "2023-01-01", "period": 86400, "valueType": "max"}#"maxeq-date": "2025-09-01"} # 
-response = requests.get(baseURL+readingURL, params=readingParams)
-if response.status_code == 200:
-    levelMeasures = pd.json_normalize(response.json()["items"])
-else:
-    raise Exception("API error {0}".format(response.status_code))
-dateCounts = levelMeasures["date"].value_counts()
-dupDates = dateCounts.index[dateCounts > 1]
-dayUIDs = levelMeasures.groupby("date").idxmax()["value"]
-dayUIDs = dayUIDs.dropna()#(pd.notnull(dayUIDs))
-measuresUnique = levelMeasures.iloc[dayUIDs,:]
-measuresUnique.plot(x="date", y="value")
+def getDataNearMaximums(maximumObs, stationID):
 
-# Zoom in  on peak
-measuresUnique = measuresUnique.sort_values('date')
-measuresUnique = measuresUnique.assign(
-    lastValue = lambda df: df.value.shift(1),
-    nextValue = lambda df: df.value.shift(-1)
-).assign(
-    localMax = lambda df: (df.value > df.lastValue) & (df.value > df.nextValue)
-)
-maximums = measuresUnique.query('localMax')
-peaks = maximums.sort_values('value', ascending=False).head(5)
-dataAroundPeaks = pd.DataFrame()
-for i in range(5):
-    peakLoc = peaks.date.iloc[i]
-    dataAroundPeak = dataNearTargetDate(nettlehamID, peakLoc)
-    dataAroundPeak = dataAroundPeak.assign(peakDate = peakLoc)
-    dataAroundPeak = dataAroundPeak.reset_index()
-    dataAroundPeaks = pd.concat([dataAroundPeaks, dataAroundPeak])
-valuesAroundPeaks = dataAroundPeaks.pivot(columns = 'peakDate', values = 'value')
-valuesAroundPeaks.plot()#x='date',y='value')
+    dataAroundPeaks = pd.DataFrame()
+    for i in range(maximumObs.shape[0]):
+        peakLoc = maximumObs.date.iloc[i]
+        dataAroundPeak = getDataNearTargetDate(stationID, peakLoc)
+        dataAroundPeak = dataAroundPeak.assign(peakDate = peakLoc)
+        dataAroundPeak = dataAroundPeak.reset_index()
+        dataAroundPeaks = pd.concat([dataAroundPeaks, dataAroundPeak])
+
+    return dataAroundPeaks
+
+
+readingURL = "/data/readings.json"
+
+if __name__ == "__main__":
+
+    nettlehamID = "a941c6ca-6b2e-45b3-a515-198c39d4ceaa"
+    levelMeasures = getReadingsData(nettlehamID)
+    measuresUnique = cleanMeasuresData(levelMeasures)
+
+    measuresUnique.plot(x="date", y="value")
+
+    peaks = getLocalMaximums(measuresUnique)
+    dataNearPeaks = getDataNearMaximums(peaks, nettlehamID)
+
+    valuesAroundPeaks = dataNearPeaks.pivot(columns = 'peakDate', values = 'value')
+    valuesAroundPeaks.plot()
